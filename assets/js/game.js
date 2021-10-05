88436b8f492cc1587d5f1aef37b45742ab9c7fbf
Ricky Leung <ricxx0320@gmail.com>
2021-10-04 18:52:49 -0700

    fix loops bugs

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,8 +18,26 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
     
-    // repeat and excute as long as thje enemy-robot is alive 
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+      } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+      }
+
     while(enemyHealth > 0) {
     //ask player if they would liek to fight or skip
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'Skip' to choose.");
